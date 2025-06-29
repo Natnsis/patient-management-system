7b8b7d04be929680d8adb4aa4578edde8d3db4fd
nati <nsisay49@gmail.com>
2025-06-29 13:59:10 -0600

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import PatientFrom from "@/components/forms/PatientFrom";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hwllo orld</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px">
+          <Image
+            src="/brand creation-bro.svg"
+            alt="image was supposed to be here"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientFrom />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ components/forms/PatientFrom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PatientFrom = () => {
+  return <div>PatientFrom</div>;
+};
+
+export default PatientFrom;
